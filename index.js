0748cf5cb04c88f1725a08c8eb5c218071841540
AlexHorbenko <agorbenko1501@gmail.com>
2022-10-11 17:41:06 +0300

    correct mistake

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const age = prompt('Please type your bithday date');
 
 const city = prompt('Please type your city');
 
-const sport = prompt('What kind of sport you prefer');
+const sport = prompt('What kind of sport do you prefer');
 
